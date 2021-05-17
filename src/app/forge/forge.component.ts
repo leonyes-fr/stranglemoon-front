353d45d3df412ea85052dd0d48de89d5897a2346
leonyes-fr <vincent.aimevent@gmail.com>
2021-05-17 22:01:17 +0200

    ajout du systeme d'upgrade premiere passe.

--- src/app/forge/forge.component.ts
+++ src/app/forge/forge.component.ts
@@ -9,7 +9,9 @@ import {HttpClient} from "@angular/common/http";
 export class ForgeComponent implements OnInit {
 
   actualRank: number = 0;
+  nextRank: any;
   name: String = '';
+  constructionInstance: any ;
 
 
   constructor(
@@ -18,15 +20,28 @@ export class ForgeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getConstructionInstances();
+
   }
 
   getConstructionInstances() {
     this.http.get<any>('http://localhost:9000/constructioninstances').subscribe(data => {
-
     this.actualRank = data[0].actualRank;
     this.name = data[0].name;
+    this.constructionInstance = data[0];
+    })
+    this.getConstructionCost();
+  }
 
-      console.log(data);
+  getConstructionCost() {
+    this.http.get<any>('http://localhost:9000/constructioncosts').subscribe(data => {
+      this.nextRank = data.find((rank : any)=> rank.rank === this.actualRank + 1);
+    })
+  }
+
+  updateTavern() {
+    this.http.put<any>('http://localhost:9000/constructioninstance/nextrank/'+ this.constructionInstance.id, { nextRank: this.nextRank }).subscribe(data => {
+      this.getConstructionInstances();
+      console.log('operation done');
     })
   }
 
